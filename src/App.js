3e73fab57becbc8e116b4d175de3528e2904514d
Jaison Miller <jaison.miller@gmail.com>
2022-04-19 10:18:22 +0100

    working framework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {
+  Box,
+  Button,
+  Collapsible,
+  Header,
+  Menu,
+  Grommet,
+  ResponsiveContext,
+  Sidebar,
+  Nav,
+} from 'grommet';
+import { Cubes, Configure, Help, Projects, Clock } from 'grommet-icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const theme = {
+  global: {
+    colors: {
+      brand: '#228BE6',
+    },
+    font: {
+      family: 'Roboto',
+      size: '14px',
+      height: '20px',
+    },
+  },
+};
+
+const AppBar = (props) => <Header background="brand" {...props} />;
+
+class App extends Component {
+  state = {
+    showSidebar: false,
+    showConfig: false,
+  };
+  render() {
+    return (
+      <Grommet theme={theme} full>
+        <ResponsiveContext.Consumer>
+          {(size) => (
+            <Box fill>
+              <AppBar>
+                <Nav gap="small" direction="row">
+                  <Button
+                    icon={<Configure />}
+                    hoverIndicator
+                    onClick={() =>
+                      this.setState({ showConfig: !this.state.showConfig })
+                    }
+                  />
+                  <Menu label="File" items={[{ label: 'new' }]} />
+                  <Box direction="row">
+                    <Menu
+                      label="Diagram"
+                      items={[
+                        { label: 'add component' },
+                        { label: 'add connection' },
+                      ]}
+                    />
+                  </Box>
+                  <Button
+                    icon={<Cubes />}
+                    hoverIndicator
+                    onClick={() =>
+                      this.setState({ showSidebar: !this.state.showSidebar })
+                    }
+                  />
+                </Nav>
+              </AppBar>
+              <Box direction="row" flex overflow={{ horizontal: 'hidden' }}>
+                <Collapsible
+                  direction="horizontal"
+                  open={this.state.showConfig}
+                >
+                  <Sidebar
+                    background="brand"
+                    footer={<Button icon={<Help />} hoverIndicator />}
+                    pad="none"
+                  >
+                    <Nav gap="small">
+                      <Button icon={<Projects />} hoverIndicator />
+                      <Button icon={<Clock />} hoverIndicator />
+                    </Nav>
+                  </Sidebar>
+                </Collapsible>
+                <Box flex align="center" justify="center">
+                  {this.state.showConfig}
+                </Box>
+                <Collapsible
+                  direction="horizontal"
+                  open={this.state.showSidebar}
+                >
+                  <Box
+                    flex
+                    width="medium"
+                    background="light-2"
+                    elevation="small"
+                    align="center"
+                    justify="center"
+                  >
+                    sidebar
+                  </Box>
+                </Collapsible>
+              </Box>
+            </Box>
+          )}
+        </ResponsiveContext.Consumer>
+      </Grommet>
+    );
+  }
 }
 
 export default App;
